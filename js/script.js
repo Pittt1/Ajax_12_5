16592da9d1fdc4845fbbf4112f2f13ccc68c21fd
Piotr Sławek <piotr.slawek@o2.pl>
2018-03-14 22:40:03 +0100

    Modify content script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var url = 'http://api.icndb.com/jokes/random';
 var tweetLink = 'https://twitter.com/intent/tweet/text=';
 var quoteUrl = 'https://quotesondesign.com/wp-json/posts/filter[orderby]=rand&filter[posts_per_page]=1';
 
+/*
+
 var $paragraph = $('#joke')
 
 var $button = $('#get-joke').on('click', function() {
@@ -17,6 +19,8 @@ function getJoke(){
 		}
 	});
 }
+
+*/
 
 function getQuote() {
 	$.getJSON(quoteUrl, createTweet);
